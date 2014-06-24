7c282019951f42381c04e2561f24f383d2950154
Thomas Beaujean <thomas@appventus.com>
2014-06-24 12:02:14 +0200

    [VICMS-230] [menu widget] l'edition du widget n'affiche pas les enfants d'enfants

--- Form/MenuType.php
+++ Form/MenuType.php
@@ -54,7 +54,31 @@ class MenuType extends AbstractType
                 'attr'        => array('class' => 'url-type'),
             ));
 
-        //we use the PRE_SUBMIT event to avoid having a circular reference
+        /*
+         * When we are editing a menu, we must add the sub menus if there are some children in the entity
+         */
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function ($event)
+            {
+                $entity = $event->getData();
+
+                if ($entity !== null) {
+                    $nbChildren = count($entity->getChildren());
+
+                    if ($nbChildren > 0) {
+                        $form = $event->getForm();
+                        $this->addChildrenField($form);
+                    }
+                }
+            }
+        );
+
+        /*
+         * we use the PRE_SUBMIT event to avoid having a circular reference
+         *
+         * This is done when a widget is created in js in the view
+         */
         $builder->addEventListener(
             FormEvents::PRE_SUBMIT,
             function ($event)
@@ -70,22 +94,30 @@ class MenuType extends AbstractType
                 //did some children was added in the form
                 if ($addChildren) {
                     $form = $event->getForm();
-                    $form->add('items', 'collection',
-                        array(
-                            'property_path' => 'children',
-                            'type' => 'victoire_form_menu',
-                            'required'     => false,
-                            'allow_add'    => true,
-                            'allow_delete' => true,
-                            'by_reference' => false
-                        )
-                    );
+                    $this->addChildrenField($form);
                 }
             }
         );
     }
 
-
+    /**
+     * Add the items field to the form
+     *
+     * @param Form $form
+     */
+    protected function addChildrenField($form)
+    {
+        $form->add('items', 'collection',
+            array(
+                'property_path' => 'children',
+                'type' => 'victoire_form_menu',
+                'required'     => false,
+                'allow_add'    => true,
+                'allow_delete' => true,
+                'by_reference' => false
+            )
+        );
+    }
     /**
      * bind form to WidgetRedactor entity
      * @param OptionsResolverInterface $resolver
--- Resources/public/edit/js/menu.js
+++ Resources/public/edit/js/menu.js
@@ -1,66 +1,101 @@
-function addSubRow(el, menuId){
-    $(el).attr('class', 'disabled');
-    var childUl = $('ul#menu-' + menuId);
-    $(childUl).show();
-    addRow($(childUl).children('a.add_menu_link'));
+/*global menus, $ */
+
+
+function Menu(el)
+{
+    this.el = el;
+    
+    var parentEl = $(el).parent().parent().parent('ul').parent().parent().parent().parent('ul');
+    
+    if (parentEl.length > 0) {
+        var parentAttrId = parentEl.attr('id');
+        this.parentId = parseInt(parentAttrId.replace('menu-', ''), 0);
+    } else {
+        this.parentId = null;
+    }
+
+    this.item = $(el).parent().parent().parent('ul').children('div').children('li').children('div').length;
+    this.newForm = '';
+    
+    this.id = this.item;
+    
+    while (menus[this.id] !== undefined) {
+        this.id = this.id + 1;
+    }
+    
+    this.index = this.item;
+    
+    console.log(this.id);
+    console.log(this.parentId);
+    
+    menus[this.id] = this;
+    
+    //get the parent by its id
+    if (this.parentId === null) {
+        this.parent = null;
+    } else {
+        this.parent = menus[this.parentId];    
+    }
 }
 
-function addRow(el){
+function addRow(el)
+{
     var menu = new Menu(el);
     menu.init();
     menu.append();
 }
 
-function deleteRow(el){
-    $(el).children('ul').each(function(elem) {
+function deleteRow(el)
+{
+    $(el).children('ul').each(function (elem) {
         var menuId = $(elem).attr('id').replace('menu-', '');
         menus[menuId] = undefined;
     });
-    if ($(el).parent('span').parent('li').parent('ul').children('li').length === 1) {
-        $(el).parent('span').parent('li').parent('ul').html('');
+    
+    if ($(el).parent('span').parent('div').parent('li').parent('ul').children('li').length === 1) {
+        $(el).parent('span').parent('div').parent('li').parent('ul').html('');
     } else {
-        $(el).parent('span').parent('li').remove();
+        $(el).parent('span').parent('div').parent('li').remove();
     }
 }
 
-function Menu(el)  {
-    this.el = el;
-    this.parentId = parseInt($(el).parents('ul').attr('id').replace('menu-', ''));
+function initMenus()
+{
+    var links = $('.add_menu_link');
+    var menu = null;
+    
+    $.each(links, function (index, link) {
+        menu = new Menu(link);
+        
+        //we update the item id with the generated one
+        $(link).parent().parent().parent('ul').attr('id', 'menu-' + menu.id);
+        //we update the index of the menu
+        menu.index = $(link).parent().parent().parent('ul').attr('data-index');
+    });
+}
 
-    this.item = $(el).parent().children('li').length;
-    this.newForm = '';
-    var cnt = 0;
-    for (var i = 0; i < menus.length; i++) {
-        if (menus[i] !== undefined) {
-            ++cnt;
-        }
-    }
-    this.id = cnt + 1;
-    menus[this.id] = this;
-    this.parent = menus[this.parentId];
-};
-
-Menu.prototype.init = function()
+Menu.prototype.init = function ()
 {
     var currentMenu = this;
     var name = '';
     var i = 0;
     do {
         i++;
-        if (currentMenu.parent === undefined) {
-            name = '[' + currentMenu.item + ']' + name;
+        if (currentMenu.parent === null) {
+            name = '[' + currentMenu.index + ']' + name;
         } else {
-            name = '[items][' + currentMenu.item + ']' + name;
+            name = '[items][' + currentMenu.index + ']' + name;
         }
         currentMenu = currentMenu.parent;
-    } while (currentMenu != null && i < 10);
+    } while (currentMenu !== null && i < 10);
 
     var newForm = prototype.replace(/\[__name__\]/g, name);
     newForm = newForm.replace(/__name__/g, name.replace('][', '_').replace('[', '').replace(']', ''));
     this.newForm = newForm.replace(/__menu_id__/g, this.id);
 };
 
-Menu.prototype.append = function()
+Menu.prototype.append = function ()
 {
-    $(this.el).before($('<li></li>').append(this.newForm));
+    $(this.el).parent('span').parent('div').parent('ul').children().first().append('<li>' + this.newForm + '</li>');
 };
+
