3dbfe5e1eede73d05d9ae0e6477f28280f4c4543
lenybernard <leny@appventus.com>
2014-08-02 18:03:58 +0200

    fix menu linkType.js (choice between page and url link type) + extends edit view

--- Resources/public/js/menu-linktype.js
+++ Resources/public/js/menu-linktype.js
@@ -1,11 +1,11 @@
-$(document).on('change', $("[id$='linkType']"), function(el) {
-    console.log(1);
-    if($(el).val() == 'page') {
-        $(el).siblings('.page-type').show();
-        $(el).siblings('.link-type').hide();
-    } else {
-        console.log($(el));
-        $(el).siblings('.page-type').hide();
-        $(el).siblings('.link-type').show();
+$vic(document).on('change', $vic("[id$='linkType']"), function(el) {
+    var select = el.target;
+
+    if($vic(select).val() == 'page') {
+        $vic(select).siblings('.page-type').show();
+        $vic(select).siblings('.url-type').hide();
+    } else if($vic(select).val() == 'url') {
+        $vic(select).siblings('.page-type').hide();
+        $vic(select).siblings('.url-type').show();
     }
 });
